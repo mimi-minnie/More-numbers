b4525593decac62a74d901131532da338c2ce646
mimi-minnie <adobbs38@gmail.com>
2014-03-06 10:27:12 +0000

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-from datetime import data as dat
+from datetime import date as dat
 
 def getYear():
     year = input ("PLease enter the year that you were born. ")
@@ -9,6 +9,7 @@ def age(yearOfBirth):
     today = dat.today()
     thisyear = today.year
     age = thisyear - yearOfBirth
+    return (age)
     
 year = getyear ()
 print("you will be %s years old this year on your birthday" %age(year))
